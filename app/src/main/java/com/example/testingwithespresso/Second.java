22859727d3742d2d55a78827973e9fa2d9500e08
LOUQLIAIMARIAM <mariamlouqliai450@gmail.com>
2023-04-04 15:49:09 +0000

    tp2

---
+++ app/src/main/java/com/example/testingwithespresso/Second.java
@@ -0,0 +1,19 @@
+package com.example.testingwithespresso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Second extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        TextView viewById = (TextView) findViewById(R.id.resultView);
+        Bundle inputData = getIntent().getExtras();
+        String input = inputData.getString("input");
+        viewById.setText(input);
+    }
+}
